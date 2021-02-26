efb773bb6a53bdecd2f06124d7c413b252b3941e
Salo <salomonco@gmail.com>
2021-02-26 23:20:34 +0100

    set up init

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from .models import User
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
@@ -15,7 +16,17 @@ db = SQLAlchemy(app)
 # Authentication
 ###########################
 
-# TODO: Add authentication setup code here!
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+from .models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+bcrypt = Bcrypt(app)
 
 
 
--- app/models.py
+++ app/models.py
@@ -1,1 +1,16 @@
-# Create your models here.
+from sqlalchemy_utils import URLType
+
+from app import db
+from flask_login import UserMixin
+
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = username = db.Column(db.String(80), nullable=False, unique=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+    
