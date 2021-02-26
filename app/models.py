434e6843c03a08dfc02a62cf915ff478efed441b
Salo <salomonco@gmail.com>
2021-02-26 23:28:52 +0100

    added model TODO

--- app/models.py
+++ app/models.py
@@ -13,4 +13,32 @@ class User(UserMixin, db.Model):
     name = username = db.Column(db.String(80), nullable=False, unique=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    #TODO:
+    # Groups Belonging - Relationship
+    # Restaurants Liked - Relationship
     
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    #TODO:
+    # Created By
+    # Name
+    # End Date
+    # Max Num of PPL
+    # Location
+    # Atendees - Relationship
+    # Restaurants - Relationship
+
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    #TODO:
+    # Created By
+    # Users who like it
+    # Location
+    # Type
+    # Price
+    # Description
+    # Picutre
+
+user_group_table = db.Table()
+user_restaurant_table = db.Table()
+group_restaurant_table = db.Table()
