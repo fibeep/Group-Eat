529f2e0ac5eaa18f5c71cee1c4c70af3ca4d567e
Salo <salomonco@gmail.com>
2021-02-28 14:49:52 +0100

    finished auth forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,8 +2,26 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, ValidationError
 from app import bcrypt
-# Import Models
+from app import User
 
-# TODO: Create Sign Up Form
+# : Create Sign Up Form
 
-# TODO: Create Log in Form
+
+class SignUpForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=3, max=50)])
+    username = StringField('User Name', validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+# : Create Log in Form
+
+
+class LoginForm(FlaskForm):
+    username = StringField('User Name', validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
--- app/models.py
+++ app/models.py
@@ -10,36 +10,45 @@ from sqlalchemy.ext.declarative import declarative_base
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = username = db.Column(db.String(80), nullable=False, unique=True)
+    name = db.Column(db.String(80), nullable=False, unique=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
     #TODO:
     # Groups Belonging - Relationship
     # Restaurants Liked - Relationship
+
     
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     #TODO:
-    # Created By
-    # Name
+    created_by = db.relationship('User')
+    name = db.Column(db.String(80), nullable=False, unique=True)
+    money_goal = db.Column(db.Float(precision=2), nullable=False)
+    location = db.Column(db.String(80), nullable=False, unique=True)
+    code = db.Column(db.String(80), nullable=False, unique=True)
     # End Date
-    # Max Num of PPL
-    # Location
-    # Special Code
     # Atendees - Relationship
     # Restaurants - Relationship
 
 class Restaurant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     #TODO:
-    # Created By
+    name = db.Column(db.String(80), nullable=False, unique=True)
+    created_by = db.relationship('User')
+    location = db.Column(db.String(80), nullable=False, unique=True)
+    type = db.Column(db.String(80), nullable=False, unique=True)
+    price_rang = db.Column(db.String(80), nullable=False, unique=True)
+    description = db.Column(db.String(), nullable=False)
+    photo_url = db.Column(URLType)
     # Users who like it
-    # Location
-    # Type
-    # Price
-    # Description
-    # Picutre
 
-user_group_table = db.Table()
-user_restaurant_table = db.Table()
-group_restaurant_table = db.Table()
+
+user_group_table = db.Table('user_group_table',
+                            db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+                            db.Column('group_id', db.Integer, db.ForeignKey('group.id')))
+user_restaurant_table = db.Table('user_restaurant_table',
+                            db.Column('user_id', db.Integer,db.ForeignKey('user.id')),
+                            db.Column('restaurant_id', db.Integer, db.ForeignKey('restaurant.id')))
+group_restaurant_table = db.Table('group_restaurant_table',
+                            db.Column('group_id', db.Integer,db.ForeignKey('group.id')),
+                            db.Column('restaurant_id', db.Integer, db.ForeignKey('restaurant.id')))
