66c5f3c39761f9679ddb4ba2d13fa097894b6adb
Salo <salomonco@gmail.com>
2021-02-28 15:31:13 +0100

    auth routes completed

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -3,7 +3,8 @@ from flask import Blueprint, request, render_template, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from datetime import date, datetime
 from app import bcrypt
-# Import Models
+from app.models import User
+from app.auth.forms import SignUpForm, LoginForm
 # Import Forms
 
 from app import app, db
@@ -14,8 +15,39 @@ auth = Blueprint('auth', __name__)
 
 # Create your routes here.
 
-# Create Signup
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    """ Route for user signup """
+    form = SignUpForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(
+            username=form.username.data,
+            password=hashed_password,
+            name = form.name.data
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash('Account Created.')
+        return redirect(url_for('auth.login'))
+    print(form.errors)
+    return render_template('signup.html', form=form)
 
-# Create Login
 
-# Create Log Out
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        login_user(user, remember=True)
+        next_page = request.args.get('next')
+        return redirect(next_page if next_page else url_for('main.homepage'))
+    print(form.errors)
+    return render_template('login.html', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.homepage'))
