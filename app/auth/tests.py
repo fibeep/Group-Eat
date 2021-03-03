848f6a71f9a83e475c317afb4de0818d851b058d
Salo <salomonco@gmail.com>
2021-03-03 17:12:05 +0100

    test 1 done

--- app/auth/tests.py
+++ app/auth/tests.py
@@ -3,19 +3,46 @@ from unittest import TestCase
 
 from datetime import date
 from app import app, db, bcrypt
-# Import Models
-
-#TODO:
-
-# tests for the signup route. It should:
-# - Make a POST request to /signup, sending a username & password
-# - Check that the user now exists in the database
+from app.models import User, Group, Restaurant
 
 
-# Tests for the signup route. It should:
-# - Create a user
-# - Make a POST request to /signup, sending the same username & password
-# - Check that the form is displayed again with an error message
+class AuthTests(TestCase):
+    """Tests for authentication (login & signup)."""
+
+    def setUp(self):
+        """Executed prior to each test."""
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        self.app = app.test_client()
+        db.drop_all()
+        db.create_all()
+
+    def test_signup_existing_user(self):
+        # Tests for the signup route. It should:
+        # - Create a user
+        # - Make a POST request to /signup, sending the same username & password
+        # - Check that the form is displayed again with an error message
+        post_data = {
+            'username': 'Test User',
+            'password': '12345',
+            'name' : 'John'
+        }
+        self.app.post('/signup', data=post_data)
+
+        post_data = {
+            'username': 'Test User',
+            'password': '12345',
+            'name': 'John'
+        }
+        response = self.app.post('/signup', data=post_data)
+
+        response_text = response.get_data(as_text=True)
+        self.assertIn(
+            'That username is taken. Please choose a different one.', response_text)
+
+
 
 # Tests for the login route. It should:
 # - Create a user
